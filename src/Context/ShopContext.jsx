86451c53b02043f88c9a0f5086d564a503f67e3f
Ashik1017 <ashikmahmud1228@gmail.com>
2024-07-02 09:04:13 +0600

    Update

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -31,14 +31,26 @@ const ShopContextProvider = (props) => {
         {
             if(cartItems[item]>0)
             {
-                let itemInfo = all_product.find((product)=>product.id===Number(item))
+                let itemInfo = all_product.find((product)=>product.id===Number(item));
                 totalAmount += itemInfo.new_price * cartItems[item];
             }
-            return totalAmount;
         }
+        return totalAmount;
     }
 
-    const contextValue = {getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
